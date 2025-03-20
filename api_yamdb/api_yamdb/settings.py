fba6a6694b4e503a38217b562d00fc75d01db1ff
Vladimir Maksimov <111915960+v-mcsimoff@users.noreply.github.com>
2025-03-20 19:43:31 +0100

    Update

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = (
-    os.getenv('SECRET_KEY'),
+    os.getenv('SECREY'),
     'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 )
 
