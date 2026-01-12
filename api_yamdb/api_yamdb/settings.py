9a0e4996751de2d28e573e6ad09f271670911174
Vladimir Maksimov <111915960+v-mcsimoff@users.noreply.github.com>
2026-01-12 16:10:31 +0100

    Update

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.
+BASE_DIR = Path(__file__).resolve()..
 
 SECRET_KEY = (
     os.getenv('SECRET_KEY'),
